68d623486584ced804669caf0f207846d6254e5e
vishal-1825 <141390140+vishal-1825@users.noreply.github.com>
2023-09-12 17:53:48 +0530

    Time: 7 ms (94.63%) | Memory: 16.6 MB (68.32%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -18,6 +18,7 @@ public:
     //     int ans=getMinCount(nums, 0, n, dp);
     //     return ans;
     // }
+    /*
     int jump(vector<int>& nums)
     {
         int n=nums.size();
@@ -34,5 +35,22 @@ public:
             dp[s]=ans;
         }
         return dp[0];
+    }
+    */
+    int jump(vector<int>& nums)
+    {
+        int n=nums.size();
+        int l=0, r=0, i, farthest, ans=0;
+
+        while (r<n-1)
+        {
+            farthest=0;
+            for (i=l; i<=r; i++)
+                farthest=max(farthest, i+nums[i]);
+            l=r+1;
+            r=farthest;
+            ans++;
+        }
+        return ans;
     }
 };
