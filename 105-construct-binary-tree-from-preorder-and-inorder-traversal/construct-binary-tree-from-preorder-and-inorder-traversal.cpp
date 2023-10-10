3b6b70f4cba7b85d178e85017e2500f51960e5e5
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-10-10 22:47:34 +0530

    Time: 7 ms (95.76%) | Memory: 26.5 MB (30.55%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+/*
     TreeNode* buildTree(vector<int> preorder, vector<int> inorder)
     {
         int n=inorder.size();
@@ -32,5 +33,27 @@ public:
         root->left=buildTree(leftPre, leftIn);
         root->right=buildTree(rightPre, rightIn);
         return root;
+    }
+*/
+    TreeNode* buildTreeHelper(vector<int>& preorder, int ps, int pe, vector<int>& inorder, int is, int ie, map<int, int>& inInd)
+    {
+        if (ps>pe || is>ie) return NULL;
+
+        int inRind=inInd[preorder[ps]];
+        int numLeft=inRind-is;
+
+        TreeNode *root = new TreeNode(preorder[ps]);
+        root->left=buildTreeHelper(preorder, ps+1, ps+numLeft, inorder, is, inRind-1, inInd);
+        root->right=buildTreeHelper(preorder, ps+numLeft+1, pe, inorder, inRind+1, ie, inInd);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        map<int, int> inInd;
+        int n=inorder.size();
+
+        for (int i=0; i<n; i++)
+            inInd[inorder[i]]=i;
+        return buildTreeHelper(preorder, 0, n-1, inorder, 0, n-1, inInd);
     }
 };
