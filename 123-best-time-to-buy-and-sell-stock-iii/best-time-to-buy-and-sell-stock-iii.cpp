ba5f44a2cfc9abcbe8ca7d17aa04a41d6b62c85b
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-10-25 12:30:15 +0530

    Time: 502 ms (49.50%) | Memory: 207.5 MB (36.01%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int maxProfit(vector<int>& prices, int i, int buy, int cap, vector<vector<vector<int>>>& dp)
     {
         if (cap==0 || i==prices.size()) return 0;
@@ -24,5 +25,25 @@ public:
     {
         vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
         return maxProfit(prices, 0, 1, 2, dp);  
+    }
+*/
+    int maxProfit(vector<int>& prices)
+    {
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        // base cae already taken care of coz all elements are 0 initially
+        for (int i=n-1; i>=0; i--) // reverse of order in recursion
+        {
+            for (int buy=0; buy<2; buy++) // reverse of order in recursion
+            {
+                for (int cap=1; cap<=2; cap++) // reverse of order in recursion
+                {
+                    if (buy) dp[i][buy][cap]=max(-prices[i]+dp[i+1][0][cap], dp[i+1][1][cap]);
+                    else dp[i][buy][cap]=max(prices[i]+dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
