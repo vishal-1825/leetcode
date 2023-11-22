a0f311fed9bab893f295f77e2cc2833e80cfcd66
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-11-22 11:24:19 +0530

    Time: 166 ms (31.37%) | Memory: 62.5 MB (12.62%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,52 +1,86 @@
 class Solution {
 public:
+    // vector<int> eventualSafeNodes(vector<vector<int>>& graph)
+    // {
+    //     int n=graph.size();
+    //     unordered_map<int, vector<int>> incoming;
+    //     vector<bool> terminal(n, false);
+    //     queue<int> terminalNodes;
+
+    //     for (int i=0; i<n; i++)
+    //     {
+    //         if (graph[i].size()==0)
+    //         {
+    //             terminal[i]=true;
+    //             terminalNodes.push(i);
+    //         }
+    //     }
+    //     for (int i=0; i<n; i++)
+    //     {
+    //         for (auto& g : graph[i])
+    //             incoming[g].push_back(i);
+    //     }
+
+    //     while (!terminalNodes.empty())
+    //     {
+    //         int node = terminalNodes.front();
+    //         terminalNodes.pop();
+
+    //         for (auto& i : incoming[node])
+    //         {
+    //             if (terminal[i]) continue;
+    //             bool flag=true;
+    //             for (auto& o : graph[i])
+    //             {
+    //                 if (!terminal[o])
+    //                 {
+    //                     flag=false;
+    //                     break;
+    //                 }
+    //             }
+    //             terminal[i]=flag;
+    //             if (terminal[i]) terminalNodes.push(i);
+    //         }
+    //     }
+
+    //     vector<int> ans;
+    //     for (int i=0; i<n; i++)
+    //     {
+    //         if (terminal[i])
+    //             ans.push_back(i);
+    //     }
+    //     return ans;
+    // }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph)
     {
         int n=graph.size();
-        unordered_map<int, vector<int>> incoming;
-        vector<bool> terminal(n, false);
-        queue<int> terminalNodes;
+        vector<vector<int>> incoming(n);
+        vector<int> outdegree(n), safe(n), ans;
+        queue<int> safeNodes;
 
         for (int i=0; i<n; i++)
         {
-            if (graph[i].size()==0)
-            {
-                terminal[i]=true;
-                terminalNodes.push(i);
-            }
-        }
-        for (int i=0; i<n; i++)
-        {
-            for (auto& g : graph[i])
+            for (int g : graph[i])
                 incoming[g].push_back(i);
+            outdegree[i]=graph[i].size();
+            if (outdegree[i]==0) safeNodes.push(i);
         }
 
-        while (!terminalNodes.empty())
+        while (!safeNodes.empty())
         {
-            int node = terminalNodes.front();
-            terminalNodes.pop();
-
-            for (auto& i : incoming[node])
+            int node = safeNodes.front();
+            safeNodes.pop();
+            safe[node]=1;
+            for (int i : incoming[node])
             {
-                if (terminal[i]) continue;
-                bool flag=true;
-                for (auto& o : graph[i])
-                {
-                    if (!terminal[o])
-                    {
-                        flag=false;
-                        break;
-                    }
-                }
-                terminal[i]=flag;
-                if (terminal[i]) terminalNodes.push(i);
+                outdegree[i]--;
+                if (outdegree[i]==0) safeNodes.push(i);
             }
         }
 
-        vector<int> ans;
         for (int i=0; i<n; i++)
         {
-            if (terminal[i])
+            if (safe[i])
                 ans.push_back(i);
         }
         return ans;
