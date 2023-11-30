c02ac611e654771a20460dc7dbbf79379183b293
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-11-30 23:45:19 +0530

    Time: 30 ms (21.13%) | Memory: 15.1 MB (17.22%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // 'visited' is not necessary here since infinite loop is not possible here as we are restricting the number of steps to k
-    int findPrice(unordered_map<int, unordered_map<int, int>>& graph, int src, int& dst, int k, vector<vector<int>>& dp)
+    int findPrice(unordered_map<int, unordered_map<int, int>>& graph, int src, int& dst, int k, vector<vector<int>>& dp, unordered_set<int>& visited)
     {
         if (src==dst) return 0;
         if (k==-1) return INT_MAX;
@@ -9,26 +9,26 @@ public:
         if (dp[src][k]!=-1) return dp[src][k];
 
         int price=INT_MAX;
-        // visited.insert(src);
+        visited.insert(src);
         for (auto& nei : graph[src])
         {
-            // if (visited.find(nei.first)!=visited.end()) continue;
-            int p=findPrice(graph, nei.first, dst, k-1, dp);
+            if (visited.find(nei.first)!=visited.end()) continue;
+            int p=findPrice(graph, nei.first, dst, k-1, dp, visited);
             if (p<INT_MAX) p+=nei.second;
             if (p<price) price=p;
         }
-        // visited.erase(src);
+        visited.erase(src);
         return dp[src][k]=price;
     }
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
     {
         unordered_map<int, unordered_map<int, int>> graph;
-        // unordered_set<int> visited;
+        unordered_set<int> visited;
         for (auto& f : flights)
             graph[f[0]][f[1]]=f[2];
 
         vector<vector<int>> dp(n, vector<int>(k+1, -1));
-        int ans=findPrice(graph, src, dst, k, dp);
+        int ans=findPrice(graph, src, dst, k, dp, visited);
         return (ans==INT_MAX ? -1 : ans);  
     }
 };
