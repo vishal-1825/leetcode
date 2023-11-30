488fdd0aefc5d9166e33ca315787c2c84afb5530
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-11-30 23:44:03 +0530

    Time: 25 ms (29.12%) | Memory: 14.5 MB (20.35%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // 'visited' is not necessary here since infinite loop is not possible here as we are restricting the number of steps to k
+    int findPrice(unordered_map<int, unordered_map<int, int>>& graph, int src, int& dst, int k, vector<vector<int>>& dp)
+    {
+        if (src==dst) return 0;
+        if (k==-1) return INT_MAX;
+
+        if (dp[src][k]!=-1) return dp[src][k];
+
+        int price=INT_MAX;
+        // visited.insert(src);
+        for (auto& nei : graph[src])
+        {
+            // if (visited.find(nei.first)!=visited.end()) continue;
+            int p=findPrice(graph, nei.first, dst, k-1, dp);
+            if (p<INT_MAX) p+=nei.second;
+            if (p<price) price=p;
+        }
+        // visited.erase(src);
+        return dp[src][k]=price;
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k)
+    {
+        unordered_map<int, unordered_map<int, int>> graph;
+        // unordered_set<int> visited;
+        for (auto& f : flights)
+            graph[f[0]][f[1]]=f[2];
+
+        vector<vector<int>> dp(n, vector<int>(k+1, -1));
+        int ans=findPrice(graph, src, dst, k, dp);
+        return (ans==INT_MAX ? -1 : ans);  
+    }
+};
