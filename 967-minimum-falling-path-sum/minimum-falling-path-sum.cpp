13eeaac707d337aa52e946d189114854e6988446
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-12-08 23:45:03 +0530

    Time: 12 ms (63.52%) | Memory: 10.6 MB (82.33%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -19,7 +19,8 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix)
     {
         int n=matrix.size(), ans=INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        // vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        vector<int> dp(n), dp2(n);
         // for (int i=0; i<n; i++)
         // {
         //     int sum = getMinSum(0, i, matrix, n, dp);
@@ -27,22 +28,23 @@ public:
         // }
         // return ans;
         for (int i=0; i<n; i++)
-            dp[n-1][i]=matrix[n-1][i];
+            dp[i]=matrix[n-1][i];
         
         for (int x=n-2; x>=0; x--)
         {
             for (int y=0; y<n; y++)
             {
                 int op1 = INT_MAX, op2 = INT_MAX, op3 = INT_MAX;
-                if (y-1>=0) op1 = dp[x+1][y-1]; // getMinSum(x+1, y-1, matrix, n, dp);
-                op2 = dp[x+1][y]; // getMinSum(x+1, y, matrix, n, dp);
-                if (y+1<n) op3 = dp[x+1][y+1]; // getMinSum(x+1, y+1, matrix, n, dp);
-                dp[x][y] = matrix[x][y] + min({op1, op2, op3});
+                if (y-1>=0) op1 = dp[y-1]; // getMinSum(x+1, y-1, matrix, n, dp);
+                op2 = dp[y]; // getMinSum(x+1, y, matrix, n, dp);
+                if (y+1<n) op3 = dp[y+1]; // getMinSum(x+1, y+1, matrix, n, dp);
+                dp2[y] = matrix[x][y] + min({op1, op2, op3});
             }
+            dp=dp2;
         }
 
         for (int i=0; i<n; i++)
-            ans=min(ans, dp[0][i]);
+            ans=min(ans, dp[i]);
         return ans;
     }
 };
