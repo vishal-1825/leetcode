08b3237348d5ab8c82dbe8876ab9326cb200d64a
Vishal <141390140+vishal-1825@users.noreply.github.com>
2023-12-18 16:16:09 +0530

    Time: 1006 ms (7.69%) | Memory: 51 MB (46.15%) - LeetSync

---
+++ 3229-minimum-cost-to-make-array-equalindromic/minimum-cost-to-make-array-equalindromic.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    // Couldn't solve without hint
+    typedef long long ll;
+    bool isPal(ll num)
+    {
+        string forward="", backward="";
+        while (num)
+        {
+            forward = char('0' + num % 10) + forward;
+            backward = backward + char('0' + num % 10);
+            num/=10;
+        }
+        return (forward==backward);
+    }
+
+    long long minimumCost(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        int n=nums.size();
+        ll median = ((n%2) ? nums[n/2] : (nums[n/2-1]+nums[n/2])/2);
+        ll leftCost=0, rightCost=0, leftPal=median, rightPal=median;
+        
+        while (!isPal(leftPal))
+            leftPal--;
+        while (!isPal(rightPal))
+            rightPal++;
+        
+        for (int i=0; i<n; i++)
+        {
+            leftCost+=abs(nums[i]-leftPal);
+            rightCost+=abs(nums[i]-rightPal);
+        }
+
+        return min(leftCost, rightCost);
+    }
+};
