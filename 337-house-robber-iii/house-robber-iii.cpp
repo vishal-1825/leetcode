6eeee2fa4e7f8926e768299dade13c53557e4e28
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-04-08 09:17:09 +0530

    Time: 4 ms (94.26%) | Memory: 24.3 MB (56.21%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -11,22 +11,41 @@
  */
 class Solution {
 public:
-    int rob(TreeNode *root, TreeNode *prev, unordered_map<TreeNode*, unordered_map<TreeNode*, int>>& dp)
+    // int rob(TreeNode *root, TreeNode *prev, unordered_map<TreeNode*, unordered_map<TreeNode*, int>>& dp)
+    // {
+    //     if (!root) return 0;
+
+    //     if (dp.find(root)!=dp.end() && dp[root].find(prev)!=dp[root].end()) return dp[root][prev];
+
+    //     int inc=INT_MIN, exc;
+    //     if (prev==NULL || (prev->left!=root && prev->right!=root)) inc=root->val+rob(root->left, root, dp)+rob(root->right, root, dp);
+    //     exc=rob(root->left, prev, dp)+rob(root->right, prev, dp);
+    //     return dp[root][prev]=max(inc, exc);
+    // }
+    // int rob(TreeNode* root)
+    // {
+    //     unordered_map<TreeNode*, unordered_map<TreeNode*, int>> dp;
+    //     return rob(root, NULL, dp);    
+    // }
+    int maxRob(TreeNode* root, unordered_map<TreeNode*, int>& dp)
     {
-        if (!root) return 0;
+        if (root==NULL) return 0;
 
-        // if (dp[root][prev]!=0) return dp[root][prev];
-        if (dp.find(root)!=dp.end() && dp[root].find(prev)!=dp[root].end()) return dp[root][prev];
+        if (dp.find(root)!=dp.end()) return dp[root];
 
-        int inc=INT_MIN, exc;
-        if (prev==NULL || (prev->left!=root && prev->right!=root)) inc=root->val+rob(root->left, root, dp)+rob(root->right, root, dp);
-        exc=rob(root->left, prev, dp)+rob(root->right, prev, dp);
-        return dp[root][prev]=max(inc, exc);
+        int inc=root->val;
+        if (root->left) inc+=maxRob(root->left->left, dp)+maxRob(root->left->right, dp);
+        if (root->right) inc+=maxRob(root->right->left, dp)+maxRob(root->right->right, dp);
+        
+        int exc=0;
+        exc+=maxRob(root->left, dp);
+        exc+=maxRob(root->right, dp);
+        
+        return dp[root]=max(inc, exc);
     }
     int rob(TreeNode* root)
     {
-        unordered_map<TreeNode*, unordered_map<TreeNode*, int>> dp;
-        return rob(root, NULL, dp);    
+        unordered_map<TreeNode*, int> dp;
+        return maxRob(root, dp);
     }
-    
 };
