c64aaa92d80d3dc62ffe6f367a13635e85a0a14b
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-04-26 12:30:15 +0530

    Time: 26 ms (86.94%) | Memory: 18 MB (67.84%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -20,23 +20,51 @@ public:
     //     vector<vector<int>> dp(row, vector<int>(row+1, -1));
     //     return mfps(grid, row, col, 0, -1, dp);
     // }
+    // T: O(n^3), S: O(n^2)
+    // int minFallingPathSum(vector<vector<int>>& grid)
+    // {
+    //     int row=grid.size(), col=grid[0].size();
+    //     vector<vector<int>> dp = grid;
+    //     int ans=INT_MAX;
+    //     for (int r=0; r<row; r++)
+    //     {
+    //         for (int c=0; c<col; c++)
+    //         {
+    //             int minval=INT_MAX;
+    //             for (int prevCol=0; r-1>=0 && prevCol<col; prevCol++)
+    //             {
+    //                 if (prevCol==c) continue;
+    //                 minval=min(minval, dp[r-1][prevCol]);
+    //             }
+    //             if (minval!=INT_MAX) dp[r][c]+=minval;
+    //             if (r==row-1) ans=min(ans, dp[r][c]);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    // T: O(n^2), S: O(n^2)
     int minFallingPathSum(vector<vector<int>>& grid)
     {
-        int row=grid.size(), col=grid[0].size();
+        int row=grid.size(), col=grid[0].size(), ans=INT_MAX;
         vector<vector<int>> dp = grid;
-        int ans=INT_MAX;
-        for (int r=0; r<row; r++)
+        for (int i=0; i<row; i++)
         {
-            for (int c=0; c<col; c++)
+            pair<int, int> smallestAbove = {INT_MAX, -1}, secondAbove = {INT_MAX, -1};
+            for (int j=0; i-1>=0 && j<col; j++)
             {
-                int minval=INT_MAX;
-                for (int prevCol=0; r-1>=0 && prevCol<col; prevCol++)
+                if (dp[i-1][j]<smallestAbove.first)
                 {
-                    if (prevCol==c) continue;
-                    minval=min(minval, dp[r-1][prevCol]);
+                    secondAbove=smallestAbove;
+                    smallestAbove={dp[i-1][j], j};
                 }
-                if (minval!=INT_MAX) dp[r][c]=grid[r][c]+minval;
-                if (r==row-1) ans=min(ans, dp[r][c]);
+                else if (dp[i-1][j]<secondAbove.first) secondAbove={dp[i-1][j], j};
+            }
+
+            for (int j=0; j<col; j++)
+            {
+                if (i>0 && smallestAbove.second==j) dp[i][j]+=secondAbove.first;
+                else if (i>0) dp[i][j]+=smallestAbove.first;
+                if (i==row-1) ans=min(ans, dp[i][j]);
             }
         }
         return ans;
