4750a0cf50a8883dfc18f8f65214c324a0641b2a
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-05-01 08:54:55 +0530

    Time: 190 ms (33.11%) | Memory: 134.6 MB (57.51%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -3,15 +3,18 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k)
     {
         int sum=0, rem;
-        unordered_map<int, pair<int, int>> count; // rem, index, count
+        unordered_map<int, int> index; // rem, index, count
+        index[0]=-1;
         for (int i=0; i<nums.size(); i++)
         {
             sum+=nums[i];
             rem=sum%k;
-            if ((rem==0 && i!=0) || (count.find(rem)!=count.end() && i-count[rem].first>1))
-                return true;
-            if (count.find(rem)==count.end()) count[rem]={i, 1};
-            else count[rem].second++;
+            if (index.find(rem)!=index.end())
+            {
+                if (i-index[rem]>1)
+                    return true;
+            }
+            else index[rem]=i;
         }
         return false;
     }
