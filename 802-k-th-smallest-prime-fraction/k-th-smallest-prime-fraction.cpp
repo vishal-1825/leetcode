d9dc09b6def92cd39c8d0cb6d4f83e64e38dc7dc
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-05-10 10:58:34 +0530

    Time: 229 ms (68.47%) | Memory: 12.3 MB (78.34%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -18,36 +18,56 @@ public:
 
     // Time complexity O(nlog(m^2)), refer to editorial for explanation
     // O(1) space
+    // vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k)
+    // {
+    //     int n=arr.size();
+    //     double left=0.0, right=1.0;
+    //     while (left<=right)
+    //     {
+    //         double fraction=(left+right)/2.0, maxfraction=-1.0; // or mid
+    //         int num=0, den=0, count=0, numi=0, deni=0;
+    //         while (numi<n-1)
+    //         {
+    //             while (deni<n && arr[numi]>=fraction*arr[deni])
+    //                 deni++;
+    //             count+=(n-deni); // count of fractions smaller than (left+right)/2
+    //             if (deni==n) break;
+    //             double frac = double(arr[numi]) / double(arr[deni]);
+    //             if (frac>maxfraction)
+    //             {
+    //                 num=arr[numi];
+    //                 den=arr[deni];
+    //                 maxfraction=frac;
+    //             }
+    //             numi++;
+    //         }
+
+    //         if (count==k) return {num, den}; // count of fractions smaller than (left+right)/2 is exactly k
+    //         if (count>k) right=fraction;
+    //         else left=fraction;
+    //     }
+    //     // Note that fraction is just greater than kth smallest fraction and not equal to it
+    //     // It's value may not exist in the array
+    //     return {};
+    // }
+
+    // Not intuitive coz we don't know that at each instant the smallest value will be between second smallest numerator/largest denominator and same numerator/second largest denominator 
+    // Time -> O((n+k)logn), Space -> O(n)
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k)
     {
+        priority_queue<pair<double, pair<int, int>>, vector<pair<double, pair<int, int>>>, greater<pair<double, pair<int, int>>>> pq;
         int n=arr.size();
-        double left=0.0, right=1.0;
-        while (left<=right)
+        for (int i=0; i<n; i++)
+            pq.push({1.0*arr[i]/arr[n-1], {i, n-1}});
+        while (k>1)
         {
-            double fraction=(left+right)/2.0, maxfraction=-1.0; // or mid
-            int num=0, den=0, count=0, numi=0, deni=0;
-            while (numi<n-1)
-            {
-                while (deni<n && arr[numi]>=fraction*arr[deni])
-                    deni++;
-                count+=(n-deni); // count of fractions smaller than (left+right)/2
-                if (deni==n) break;
-                double frac = double(arr[numi]) / double(arr[deni]);
-                if (frac>maxfraction)
-                {
-                    num=arr[numi];
-                    den=arr[deni];
-                    maxfraction=frac;
-                }
-                numi++;
-            }
-
-            if (count==k) return {num, den}; // count of fractions smaller than (left+right)/2 is exactly k
-            if (count>k) right=fraction;
-            else left=fraction;
+            int numi=pq.top().second.first;
+            int deni=pq.top().second.second;
+            pq.pop();
+            deni--;
+            pq.push({1.0*arr[numi]/arr[deni], {numi, deni}});
+            k--;
         }
-        // Note that fraction is just greater than kth smallest fraction and not equal to it
-        // It's value may not exist in the array
-        return {};
+        return {arr[pq.top().second.first], arr[pq.top().second.second]};
     }
 };
