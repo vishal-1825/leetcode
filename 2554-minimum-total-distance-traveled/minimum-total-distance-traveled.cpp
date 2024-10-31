7b7c4dfee6d416a20545ae46922960852661e21c
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-10-31 21:38:18 +0530

    Time: 63 ms (62.07%) | Memory: 68 MB (23.13%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    long long minDist(vector<int>& robot, vector<int>& factories, int i, int j, vector<vector<long long>>& dp)
-    {
-        if (i==robot.size()) return 0;
-        if (j==factories.size()) return LLONG_MAX;
+    // long long minDist(vector<int>& robot, vector<int>& factories, int i, int j, vector<vector<long long>>& dp)
+    // {
+    //     if (i==robot.size()) return 0;
+    //     if (j==factories.size()) return LLONG_MAX;
 
-        if (dp[i][j]!=-1) return dp[i][j];
+    //     if (dp[i][j]!=-1) return dp[i][j];
 
-        long long inc = minDist(robot, factories, i+1, j+1, dp);
-        if (inc!=LLONG_MAX) inc+=abs(robot[i]-factories[j]);
-        long long exc = minDist(robot, factories, i, j+1, dp);
-        return dp[i][j] = min(inc, exc);
-    }
+    //     long long inc = minDist(robot, factories, i+1, j+1, dp);
+    //     if (inc!=LLONG_MAX) inc+=abs(robot[i]-factories[j]);
+    //     long long exc = minDist(robot, factories, i, j+1, dp);
+    //     return dp[i][j] = min(inc, exc);
+    // }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory)
     {
         vector<int> factories;
@@ -22,7 +22,20 @@ public:
         }
         sort(robot.begin(), robot.end());
         sort(factories.begin(), factories.end());
-        vector<vector<long long>> dp(robot.size(), vector<long long>(factories.size(), -1));
-        return minDist(robot, factories, 0, 0, dp);
+        vector<vector<long long>> dp(robot.size()+1, vector<long long>(factories.size()+1, 0));
+        // return minDist(robot, factories, 0, 0, dp);
+        for (int i=0; i<robot.size(); i++)
+            dp[i][factories.size()]=LLONG_MAX;
+        for (int i=robot.size()-1; i>=0; i--)
+        {
+            for (int j=factories.size()-1; j>=0; j--)
+            {
+                long long inc = dp[i+1][j+1];
+                if (inc!=LLONG_MAX) inc+=abs(robot[i]-factories[j]);
+                long long exc = dp[i][j+1];
+                dp[i][j] = min(inc, exc);
+            }
+        }
+        return dp[0][0];
     }
 };
