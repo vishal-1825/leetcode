73dbf97003bd6a9165a59d29ae4e0932ee4b05ef
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-11-08 20:35:15 +0530

    Time: 53 ms (14.99%) | Memory: 38.1 MB (7.18%) - LeetSync

---
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    // dijkstra
+    int minTimeToReach(vector<vector<int>>& moveTime)
+    {
+        int n=moveTime.size(), m=moveTime[0].size();
+        vector<vector<int>> minTime(n, vector<int>(m, INT_MAX));
+        minTime[0][0]=0;
+        vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({0, {0, 0}});
+        while (!pq.empty())
+        {
+            int timeSoFar=pq.top().first;
+            int i=pq.top().second.first;
+            int j=pq.top().second.second;
+            pq.pop();
+            for (auto d: dir)
+            {
+                int x=i+d[0], y=j+d[1];
+                if (x>=0 && x<n && y>=0 && y<m)
+                {
+                    int time=max(timeSoFar, moveTime[x][y])+1;
+                    if (time<minTime[x][y])
+                    {
+                        minTime[x][y]=time;
+                        pq.push({time, {x, y}});
+                    }
+                }
+            }
+        }
+        return minTime[n-1][m-1];
+    }
+};
