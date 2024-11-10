ab5c396fc079988f068c11fca37fc8ba52811720
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-11-10 09:39:22 +0530

    Time: 35 ms (91.21%) | Memory: 88.2 MB (89.97%) - LeetSync

--- 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    int orval(vector<int> contribution)
-    {
-        int val=0;
-        for (int k=0; k<=31; k++)
-            val+=(contribution[k] ? 1 : 0)*(1<<k);
-        return val;
-    }
-
-    void addContribution(int val, vector<int>& contribution, bool isadd)
-    {
-        int k=0;
-        while (val>0)
-        {
-            contribution[k]=contribution[k]+(isadd ? 1 : -1)*val%2;
-            val/=2;
-            k++;
-        }
-    }
-
     int minimumSubarrayLength(vector<int>& nums, int k)
     {
-        if (k==0) return 1;
-
-        int n=nums.size(), i=0, j=0, ans=INT_MAX;
-        vector<int> contribution(32, 0);
-        while (j<n)
+        int bor=0, left=0, ans=INT_MAX;
+        vector<int> ones(32, 0);
+        for (int right=0; right<nums.size(); right++)
         {
-            addContribution(nums[j], contribution, true);
-            while (orval(contribution)>=k && i<=j)
+            bor|=nums[right];
+            for (int i=0; i<=31; i++)
+                ones[i]+=(nums[right]>>i & 1);
+            while (left<=right && bor>=k)
             {
-                ans=min(ans, j-i+1);
-                addContribution(nums[i], contribution, false);
-                i++;
+                ans=min(ans, right-left+1);
+                for (int i=0; i<=31; i++)
+                {
+                    if (nums[left]>>i & 1)
+                    {
+                        ones[i]--;
+                        if (ones[i]==0)
+                            bor^=(1<<i);
+                    }
+                }
+                left++;
             }
-            j++;
         }
         return (ans==INT_MAX ? -1 : ans);
     }
