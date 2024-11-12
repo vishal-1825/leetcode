ad06b335289ed73dd0ab88a3e2eb8891b4646961
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-11-12 10:20:42 +0530

    Time: 59 ms (58.20%) | Memory: 92.2 MB (85.47%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,25 +1,54 @@
 class Solution {
 public:
+    int binarySearch(int maxPrice, vector<vector<int>>& items)
+    {
+        int l=0, r=items.size()-1, maxBeauty=0;
+        while (l<=r)
+        {
+            int m=(l+r)/2;
+            if (items[m][0]<=maxPrice)
+            {
+                maxBeauty=max(maxBeauty, items[m][1]);
+                l=m+1;
+            }
+            else r=m-1;
+        }
+        return maxBeauty;
+    }
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries)
     {
         sort(items.begin(), items.end());
-        map<int, int> q;
-        for (int i=0; i<queries.size(); i++)
-            q[queries[i]]=0;
-        int i=0, maxBeauty=0;
-        for (auto query: q)
+        int maxBeauty=items[0][1];
+        for (int i=0; i<items.size(); i++)
         {
-            int price=query.first;
-            while (i<items.size() && items[i][0]<=price)
-            {
-                maxBeauty=max(maxBeauty, items[i][1]);
-                i++;
-            }
-            q[price]=maxBeauty;
+            maxBeauty=max(maxBeauty, items[i][1]);
+            items[i][1]=maxBeauty;
         }
         vector<int> ans;
         for (int i=0; i<queries.size(); i++)
-            ans.push_back(q[queries[i]]);
+            ans.push_back(binarySearch(queries[i], items));
         return ans;
     }
+    // vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries)
+    // {
+    //     sort(items.begin(), items.end());
+    //     map<int, int> q;
+    //     for (int i=0; i<queries.size(); i++)
+    //         q[queries[i]]=0;
+    //     int i=0, maxBeauty=0;
+    //     for (auto query: q)
+    //     {
+    //         int price=query.first;
+    //         while (i<items.size() && items[i][0]<=price)
+    //         {
+    //             maxBeauty=max(maxBeauty, items[i][1]);
+    //             i++;
+    //         }
+    //         q[price]=maxBeauty;
+    //     }
+    //     vector<int> ans;
+    //     for (int i=0; i<queries.size(); i++)
+    //         ans.push_back(q[queries[i]]);
+    //     return ans;
+    // }
 };
