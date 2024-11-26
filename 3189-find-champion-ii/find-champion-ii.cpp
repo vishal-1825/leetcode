8126e78c4a2f1d035b6a6c1194660e54aee2110e
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-11-26 09:53:30 +0530

    Time: 7 ms (70.56%) | Memory: 93.2 MB (87.31%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.cpp
+++ 3189-find-champion-ii/find-champion-ii.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges)
     {
-        int m=edges.size(), ans, count=0;
-        vector<int> pointingCount(n, 0);
-        for (int i=0; i<m; i++)
-            pointingCount[edges[i][1]]++;
+        vector<int> indegree(n, 0);
+        for (int i=0; i<edges.size(); i++)
+            indegree[edges[i][1]]++;
+        
+        int champion=-1;
         for (int i=0; i<n; i++)
         {
-            if (pointingCount[i]==0)
+            if (indegree[i]==0)
             {
-                if (count==1) return -1;
-                count++;
-                ans=i;
+                if (champion==-1) champion=i;
+                else return -1;
             }
         }
-        return ans;
+        return champion;
     }
 };
