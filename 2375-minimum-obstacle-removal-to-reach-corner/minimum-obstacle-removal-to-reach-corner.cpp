2af2d9b080445919bdaa303c943ef7ad3e3aee9b
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-11-28 17:59:40 +0530

    Time: 1278 ms (12.35%) | Memory: 448.7 MB (8.62%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,53 +1,66 @@
 class Solution {
 public:
-    // vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    // int minObst(int i, int j, int& m, int& n, vector<vector<int>>& grid, vector<vector<bool>>& vis)
+    // int minimumObstacles(vector<vector<int>>& grid)
     // {
-    //     if (i==m-1 && j==n-1) return 0;
-    //     vis[i][j]=true;
-    //     int ans=m*n;
-    //     for (auto d: dir)
+    //     int m=grid.size(), n=grid[0].size();
+    //     vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    //     vector<vector<int>> minObstacles(m, vector<int>(n, INT_MAX));
+    //     minObstacles[0][0]=0;
+    //     priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+    //     pq.push({0, 0, 0});
+    //     while (!pq.empty())
     //     {
-    //         int x=i+d[0], y=j+d[1];
-    //         if (x>=0 && x<m && y>=0 && y<n && !vis[x][y])
+    //         int obstacles=pq.top()[0], i=pq.top()[1], j=pq.top()[2];
+    //         pq.pop();
+    //         if (i==m-1 && j==n-1) return obstacles;
+    //         for (auto d: dir)
     //         {
-    //             int removal=minObst(x, y, m, n, grid, vis);
-    //             if (grid[x][y]==1) removal++;
-    //             ans=min(ans, removal);
-    //         } 
+    //             int x=i+d[0], y=j+d[1];
+    //             if (x>=0 && x<m && y>=0 && y<n)
+    //             {
+    //                 int totalObstacles=obstacles+grid[x][y];
+    //                 if (totalObstacles<minObstacles[x][y])
+    //                 {
+    //                     pq.push({totalObstacles, x, y});
+    //                     minObstacles[x][y]=totalObstacles;
+    //                 }
+    //             }
+    //         }
     //     }
-    //     vis[i][j]=false;
-    //     return dp[i][j]=ans;
+    //     return -1;
     // }
     int minimumObstacles(vector<vector<int>>& grid)
     {
-        // vector<vector<bool>> vis(m, vector<bool>(n, false));
-        // return minObst(0, 0, m, n, grid, vis);
         int m=grid.size(), n=grid[0].size();
-        vector<vector<int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         vector<vector<int>> minObstacles(m, vector<int>(n, INT_MAX));
         minObstacles[0][0]=0;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0, 0, 0});
-        while (!pq.empty())
+        deque<vector<int>> dq;
+        dq.push_back({0, 0, 0});
+        while (!dq.empty())
         {
-            int obstacles=pq.top()[0], i=pq.top()[1], j=pq.top()[2];
-            pq.pop();
+            int obstacles=dq.front()[0], i=dq.front()[1], j=dq.front()[2];
+            dq.pop_front();
             if (i==m-1 && j==n-1) return obstacles;
             for (auto d: dir)
             {
                 int x=i+d[0], y=j+d[1];
-                if (x>=0 && x<m && y>=0 && y<n)
+                if (x>=0 && x<m && y>=0 && y<n && minObstacles[x][y]==INT_MAX)
                 {
-                    int totalObstacles=obstacles+grid[x][y];
-                    if (totalObstacles<minObstacles[x][y])
+                    if (grid[x][y]==1)
                     {
-                        pq.push({totalObstacles, x, y});
-                        minObstacles[x][y]=totalObstacles;
+                        minObstacles[x][y]=obstacles+1;
+                        dq.push_back({obstacles+1, x, y});
+                    }
+                    else
+                    {
+                        minObstacles[x][y]=obstacles;
+                        dq.push_front({obstacles, x, y});
                     }
                 }
             }
         }
         return -1;
+        // return minObstacles[m-1][n-1];
     }
 };
