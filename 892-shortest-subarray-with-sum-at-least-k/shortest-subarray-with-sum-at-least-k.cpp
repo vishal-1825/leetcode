270e8fc40fec9af3fdce4bf7c31b3fca85ec29e2
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-12-03 12:01:38 +0530

    Time: 40 ms (55.65%) | Memory: 113.8 MB (26.56%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+    // int shortestSubarray(vector<int>& nums, int k)
+    // {
+    //     int n=nums.size(), ans=n+1;
+    //     long long sum=0;
+    //     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+    //     for (int i=0; i<n; i++)
+    //     {
+    //         sum+=nums[i];
+    //         if (sum>=k) ans=min(ans, i+1);
+    //         while (!pq.empty() && sum-pq.top().first>=k)
+    //         {
+    //             ans=min(ans, i-pq.top().second);
+    //             pq.pop();
+    //         }
+    //         pq.push({sum, i});
+    //     }
+    //     return (ans==n+1 ? -1 : ans);
+    // }
     int shortestSubarray(vector<int>& nums, int k)
     {
         int n=nums.size(), ans=n+1;
         long long sum=0;
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        deque<pair<long long, int>> dq;
         for (int i=0; i<n; i++)
         {
             sum+=nums[i];
             if (sum>=k) ans=min(ans, i+1);
-            while (!pq.empty() && sum-pq.top().first>=k)
+            while (!dq.empty() && sum-dq.front().first>=k)
             {
-                ans=min(ans, i-pq.top().second);
-                pq.pop();
+                ans=min(ans, i-dq.front().second);
+                dq.pop_front();
             }
-            pq.push({sum, i});
+            while (!dq.empty() && sum<dq.back().first)
+                dq.pop_back();
+            dq.push_back({sum, i});
         }
         return (ans==n+1 ? -1 : ans);
     }
