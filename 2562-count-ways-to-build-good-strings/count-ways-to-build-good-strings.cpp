11a8c70f5f180f8b843d83559db4b3699e18fb15
Vishal <141390140+vishal-1825@users.noreply.github.com>
2024-12-30 08:43:20 +0530

    Time: 3 ms (96.09%) | Memory: 14.6 MB (52.05%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     long long mod=1e9+7;
-    long long countGoodStrings(vector<long long>& dp, int zero, int one, int low, int high, int count)
-    {
-        if (count>high) return 0;
-        if (dp[count]!=-1) return dp[count];
-        long long addOne=countGoodStrings(dp, zero, one, low, high, count+one);
-        long long addZero=countGoodStrings(dp, zero, one, low, high, count+zero);
-        long long addNothing=((count>=low && count<=high) ? 1 : 0);
-        return dp[count]=(addOne+addZero+addNothing)%mod;
-    }
+    // long long countGoodStrings(vector<long long>& dp, int zero, int one, int low, int high, int count)
+    // {
+    //     if (count>high) return 0;
+    //     if (dp[count]!=-1) return dp[count];
+    //     long long addOne=countGoodStrings(dp, zero, one, low, high, count+one);
+    //     long long addZero=countGoodStrings(dp, zero, one, low, high, count+zero);
+    //     long long addNothing=((count>=low && count<=high) ? 1 : 0);
+    //     return dp[count]=(addOne+addZero+addNothing)%mod;
+    // }
     int countGoodStrings(int low, int high, int zero, int one)
     {
-        vector<long long> dp(high+1, -1);
-        return (int)(countGoodStrings(dp, zero, one, low, high, 0)%mod);
+        // vector<long long> dp(high+1, -1);
+        // return (int)(countGoodStrings(dp, zero, one, low, high, 0)%mod);
+        vector<long long> dp(high+1);
+        dp[0]=1;
+        for (int i=1; i<=high; i++)
+        {
+            if (i>=zero) dp[i]=(dp[i]+dp[i-zero])%mod;
+            if (i>=one) dp[i]=(dp[i]+dp[i-one])%mod;
+        }
+
+        long long ans=0;
+        for (int i=low; i<=high; i++)
+            ans=(ans+dp[i])%mod;
+        return ans;
     }
 };
